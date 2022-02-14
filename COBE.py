d19e49d3d5f151525e1fec43633b3cd5863f87cd
Chigozieworldwide <84646942+Chigozieworldwide@users.noreply.github.com>
2022-02-14 15:42:26 +0100

    COBE.py

---
+++ COBE.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+# Update V1.6
+
+### Import Module
+import os
+try:
+    import requests
+except ImportError:
+    print('\n [×] Modul requests belum terinstall!...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [×] Modul Futures belum terinstall!...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [×] Modul Bs4 belum terinstall!...\n')
+    os.system('pip install bs4')
+
+import requests, os, re, bs4, sys, json, time, random, datetime, subprocess
+from concurrent.futures import ThreadPoolExecutor as YayanGanteng
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def xoshnaw():
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "-".join(uuid)
+  print("\x1b[37;1mYOUR ID : "+id)
+  try:
+    httpCaht = requests.get("https://raw.githubusercontent.com/Chigozieworldwide/COBE/main/List.txt").text
+    if id in httpCaht:
+      print("\033[1;92mYOUR ID IS ACTIVE...!")
+      msg = str(os.geteuid())
+      time.sleep(0.3)
+      pass
+    else:
+      print("\x1b[1;91mID ACTIVATE (telegram) INBOX  @CHIGOZIEWORLDWIDE")
+      os.system('xdg-open https://wa.me/+2348069472717')
+      time.sleep(1)
+      sys.exit()
+  except:
+    sys.exit()
+    if name == '__main__':
+    	print(logo)
+    	xoshnaw()
+xoshnaw()
+ct = datetime.now()
+n = ct.month
+import os
+
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+       __import__("COBE").moch_yayan()
+   except Exception as e:
+       exit(str(e))
